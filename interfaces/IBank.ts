8b85ce5fb04b7e1bcfa73d40fccd7077d997cc9d
Vedang <vedang.wappnet@gmail.com>
2025-02-04 16:49:26 +0530

    implemented Bank

--- interfaces/IBank.ts
+++ interfaces/IBank.ts
@@ -3,9 +3,6 @@ import { IAccount } from "./IAccount";
 export interface IBank{
     bankName: string,
     accounts: IAccount[]
-    
-    getBankName(): string;
-    setBankName(): string;
-    getAccounts(): IAccount[];
+
     addAccount(account: IAccount): void;
 }
