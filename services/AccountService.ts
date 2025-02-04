8fa7639d65d19a5db9f6ab4f99bd802f9259f626
Vedang <vedang.wappnet@gmail.com>
2025-02-04 17:31:09 +0530

    changes made in naming conventions

---
+++ services/AccountService.ts
@@ -0,0 +1,13 @@
+import { CurrentAccount } from "../classes/CurrentAccount";
+import { SavingsAccount } from "../classes/SavingsAccount";
+
+export class AccountService {
+    public createAccount(pin: number, initialBalance: number = 0, accType: number): Account {
+        if(accType == 1){
+            return new SavingsAccount(pin, initialBalance);
+        }
+        else if(accType == 2){
+            return new CurrentAccount(pin, initialBalance);
+        }
+    }
+}
--- services/AuthService.ts
+++ services/AuthService.ts
@@ -1,4 +1,4 @@
-import { User } from "../models/User";
+import { User } from "../classes/User";
 
 export class AuthService {
     private users: User[] = [];
