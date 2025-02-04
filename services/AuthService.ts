9da8428200bb40d5202d3ddb586015b1a8bd404d
Vedang <vedang.wappnet@gmail.com>
2025-02-04 17:02:31 +0530

    authService for register and login is created.

---
+++ services/AuthService.ts
@@ -0,0 +1,17 @@
+import { User } from "../models/User";
+
+export class AuthService {
+    private users: User[] = [];
+
+    public register(username: string, password: string): User {
+        const user = new User(username, password);
+        this.users.push(user);
+        console.log("User is registered successfully");
+        return user;
+    }
+
+    public login(username: string, password: string): User | null {
+        const user = this.users.find(u => u.username === username && u.password === password);
+        return user || null;
+    }
+}
