46674cf615a9fed490bd7fb164c69e1e2832f4e7
Vedang <vedang.wappnet@gmail.com>
2025-02-05 14:23:18 +0530

    UPIPayment is implemented

---
+++ services/UPIPayment.ts
@@ -0,0 +1,7 @@
+import { Payment } from "../interfaces/IPayment";
+
+export class UPIPayment implements Payment{
+    pay(amount: number): void {
+        console.log(`Paid ${amount} using Credit Card.`);
+    }
+}
